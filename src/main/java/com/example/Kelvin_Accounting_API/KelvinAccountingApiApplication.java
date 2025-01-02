708d197e2c69a49f2ac66760748a6bd6e38046b6
Kelvin Lin <kelvin@bu.edu>
2025-01-02 13:18:12 -0500

    initial spring files

---
+++ src/main/java/com/example/Kelvin_Accounting_API/KelvinAccountingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Kelvin_Accounting_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelvinAccountingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelvinAccountingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Kelvin_Accounting_API/KelvinAccountingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Kelvin_Accounting_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelvinAccountingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
